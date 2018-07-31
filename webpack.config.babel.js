651ae78a9c99ca15b1b9f5ccae349088bf6fbddf
Srinath <srinath.sathianarayanan@iff.com>
2018-07-31 19:04:45 +0530

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,118 @@
+import path from 'path';
+import webpack from 'webpack';
+import fs from 'fs';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import cssnano from 'cssnano';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  mode: process.env.NODE_ENV,
+  devtool: IS_PROD ? 'source-map' : false,
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    index: 'index.html',
+    open: true,
+    stats: 'errors-only',
+    watchContentBase: true
+  },
+  entry: './scripts/index.js',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [IS_PROD ? MiniCssExtractPlugin.loader : 'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            require('stylelint'),
+            require('postcss-preset-env')
+          ]
+        }
+      }],
+    }, {
+      test: /\.(png|jpg)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]',
+          publicPath: '/'
+        }
+      }
+    }, {
+      test: /\.(ttf|eot|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+          publicPath: '../'
+        }
+      },
+    }]
+  },
+  output: {
+    filename: IS_PROD ? 'scripts/[name].[hash:6].js' : 'scripts/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'initial',
+          minSize: 0,
+          minChunks: 3
+        },
+      },
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      title: 'Welcome to starter kit',
+      hash: true
+    }),
+    new webpack.HotModuleReplacementPlugin({
+
+    })
+  ]
+};
+
+if (IS_PROD) {
+  config.plugins.push(
+    // new UglifyJSPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[hash:6].min.css',
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+        safe: true
+      },
+      canPrint: false,
+    })
+  );
+}
+
+export default config;
